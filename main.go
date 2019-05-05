b3542e38d5d798d426ea1196d98232170ac5b218
dtext <textores.danny@gmail.com>
2019-05-05 22:02:09 +0200

    Build basic crawler

--- main.go
+++ main.go
@@ -3,11 +3,23 @@ package main
 import (
 	"fmt"
 	"github.com/docopt/docopt-go"
+	"strings"
 )
 
 const(
 	version string = "0.1"
 )
+
+func (w *Website) String() string {
+	var sb strings.Builder
+	sb.WriteString(w.Url)
+	sb.WriteString(" links the following websites:\n")
+	for _, link := range w.LinkedSites {
+		sb.WriteString("  ")
+		sb.WriteString(link.Url)
+	}
+	return sb.String()
+}
 
 func main() {
 	usage := `gora, the web explorer. Starting from a given URL, crawls the web and creates a graph model of connected sites.
@@ -30,6 +42,7 @@ Options:
 	versionArg, err := arguments.Bool("--version")
 	if err == nil && versionArg {
 		fmt.Printf("gora version %s", version)
+		return
 	}
 
 	url, err := arguments.String("<url>")
@@ -37,4 +50,23 @@ Options:
 		fmt.Println(usage)
 		return
 	}
+
+
+	fmt.Printf("Starting from: %v\n", url)
+	limit, _ := arguments.Int("--max")
+	timeout, _ := arguments.Int("--timeout")
+
+	if limit == 0 && timeout == 0 {
+		fmt.Println("At least one of the options --max and --timeout needs to be set to a positive integer.")
+		return
+	}
+
+	web := BuildGraph(url, limit, timeout, func(website *Website) {
+		fmt.Printf("Processed: %s\n", website.Url)
+	})
+	fmt.Printf("The Web, starting from %s\n\n", url)
+	for _, website := range web {
+		fmt.Println(website)
+	}
+
 }
--- web.go
+++ web.go
@@ -4,22 +4,127 @@ import (
 	"io/ioutil"
 	"net/http"
 	"regexp"
+	"sync"
+	"time"
 )
 
 type Website struct {
 	LinkedSites []*Website
-	Loading bool
-	Retrieved bool
-	Url string
-	Protocol string
+	Loading     bool
+	Retrieved   bool
+	Url         string
+	lock        sync.Mutex
+	err         error
 }
 
-var urlRegex = regexp.MustCompile(`^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?`)
+type Web map[string]Website
 
-func (w *Website) RetrieveLinkedSites() {
+var urlRegex = regexp.MustCompile(`https?://([a-zA-Z0-9]*\.)([a-zA-Z0-9]*\.?)*/?`)
+
+
+
+func (w *Website) acquireOkForLoad() bool {
+	// acquire lock, release when done
+	w.lock.Lock()
+	defer w.lock.Unlock()
+
+	// website is ok for load if it's neither retrieved nor loading
+	ok := !w.Retrieved && !w.Loading
+	if !ok {
+		return false
+	}
+	w.Loading = true
+	return true
+}
+
+func BuildGraph(startingUrl string, limit, timeout int, resultHandler func(*Website)) Web {
+	theWeb := make(Web)
+	results := make(chan *Website)
+	work := make(chan *Website)
+
+	w := Website{LinkedSites: []*Website{},
+		Loading:   true,
+		Retrieved: false,
+		Url:       startingUrl,
+		err:       nil,
+		lock:      sync.Mutex{},
+	}
+	theWeb[startingUrl] = w
+
+	// Timeout after <timeout> seconds or never
+	var timeoutChannel = make(chan time.Time)
+	go func() {
+		if timeout == 0 {
+			return
+		}
+		time.Sleep(time.Duration(timeout) * time.Second)
+		timeoutChannel <- time.Now()
+	}()
+
+	// Kickoff
+	go w.RetrieveAsync(&theWeb, results, work)
+
+	// Wait for timeout, more websites to fetch and results
+	select {
+	case <-timeoutChannel:
+		// TODO stop goroutines
+		break
+	case website := <-work:
+		if limit == 0 {
+			break
+		}
+		limit--
+		go website.RetrieveAsync(&theWeb, results, work)
+	case res := <-results:
+		resultHandler(res)
+	}
+	return theWeb
+}
+
+func (w *Website) RetrieveAsync(theWeb *Web, results chan *Website, work chan *Website) {
+	// get HTML, filter out URLS, build Website structs
+	w.RetrieveLinks(theWeb)
+
+	// done fetching: send to results channel
+	results <- w
+
+	// send unfetched links to work channel
+	for _, website := range w.LinkedSites {
+		if !website.acquireOkForLoad() {
+			continue
+		}
+		work <- website
+	}
+}
+
+func (w *Website) RetrieveLinks(theWeb *Web) {
+	// HTTP(S) GET
 	body, err := w.get()
-	if err == nil {
+	if err != nil {
+		w.err = err
+		return
+	}
 
+	// find outgoing URLs
+	outgoingLinks := urlRegex.FindAllString(body, -1)
+	for _, link := range outgoingLinks {
+		// do we have that URL already?
+		website, ok := (*theWeb)[link]
+
+		// if not, create a Website struct for it and save that
+		if !ok {
+			website = Website{
+				LinkedSites: make([]*Website, 4),
+				Loading:     false,
+				Retrieved:   false,
+				Url:         link,
+				lock:        sync.Mutex{},
+				err:         nil,
+			}
+			(*theWeb)[link] = website
+		}
+		// TODO use map for multiple link support
+		w.LinkedSites = append(w.LinkedSites, &website)
 	}
 }
 
@@ -35,8 +140,4 @@ func (w *Website) get() (string, error) {
 	}
 
 	return string(body), nil
-}
-
-func (w *Website) findOutgoingLinks(body [][]string) {
-	return urlRegex.FindAllStringSubmatch(body, 4)
 }
